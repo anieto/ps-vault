3d4b4946d4b0c159105465d89fa5a116be19d6bb
Adam Nieto <adamnieto@hey.com>
2026-05-26 17:55:31 -0500

    Move AdminSection above SessionsSection in settings page

--- web/src/app/(app)/settings/page.tsx
+++ web/src/app/(app)/settings/page.tsx
@@ -56,8 +56,8 @@ export default function SettingsPage() {
       <SwitchSection />
       <AccountSection />
       <SecuritySection />
-      <SessionsSection />
       <AdminSection />
+      <SessionsSection />
     </div>
   );
 }
